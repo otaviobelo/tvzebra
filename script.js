b2a198bddab2e64c09c8acbd72ce97cdd27d28b4
otaviobelo <108193717+otaviobelo@users.noreply.github.com>
2025-03-04 11:12:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Inicializa os players
+document.addEventListener('DOMContentLoaded', function() {
+    var player1 = videojs('stream1');
+    var player2 = videojs('stream2');
+    var player3 = videojs('stream3');
+});
